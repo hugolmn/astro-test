7cec2e37e397fe6bd4064e0e0e1d9edf7c688e69
Hugo Le Moine <hugo.le-moine@outlook.fr>
2023-05-22 19:29:57 +0200

    Added astro-seo, tailwindcss

--- astro.config.mjs
+++ astro.config.mjs
@@ -1,7 +1,10 @@
 import { defineConfig } from 'astro/config';
 
+import tailwind from "@astrojs/tailwind";
+
 // https://astro.build/config
 export default defineConfig({
-    site: 'https://hugolmn.github.io',
-    base: '/astro-test'
+  site: 'https://hugolmn.github.io',
+  base: '/astro-test',
+  integrations: [tailwind()]
 });
